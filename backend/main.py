04063c067e99bfe4ae619e57bac05b073654fab4
chrishart0 <christianphart0@gmail.com>
2024-01-01 14:24:11 -0500

    break out DB calls to a helper file

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,9 @@ from botocore.exceptions import ClientError
 from datetime import datetime
 import uuid
 
+# Utils
+import services.db_operations as db
+
 app = FastAPI()
 
 origins = [
@@ -27,81 +30,33 @@ app.add_middleware(
 dynamodb = boto3.resource('dynamodb', endpoint_url="http://dynamodb-local:8000")
 table = dynamodb.Table('ToDoItems')
 
+
 @app.post("/todos")
 async def create_todo(todo: ToDoItem):
-    try:
-        # Add created and updated datestamps filter date to just the second
-        item = todo.model_dump()
-        item['id'] = str(uuid.uuid4())
-        item['created_at'] = datetime.utcnow().isoformat().split('.')[0]
-        item['updated_at'] = datetime.utcnow().isoformat().split('.')[0]
-        item['owner'] = "chris"
-        print(item)
-        response = table.put_item(Item=item)
-        return item
-    except ClientError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+
+    # Add created and updated datestamps filter date to just the second
+    item = todo.model_dump()
+    item['id'] = str(uuid.uuid4())
+    item['created_at'] = datetime.utcnow().isoformat().split('.')[0]
+    item['updated_at'] = datetime.utcnow().isoformat().split('.')[0]
+    item['owner'] = "chris"
+    print(item)
+    db.create_todo(item)
+
+    return item
 
 @app.get("/todos")
 async def get_all_todos():
-    try:
-        response = table.scan()
-        return response.get('Items', [])
-    except ClientError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    return db.get_all_todos()
 
 @app.get("/todos/{id}")
 async def get_todo(id: str):
-    try:
-        response = table.get_item(Key={'id': id})
-        item = response.get('Item')
-        if item:
-            return item
-        raise HTTPException(status_code=404, detail="Item not found")
-    except ClientError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    return db.get_todo(id)
 
 @app.put("/todos/{id}")
 async def update_todo(id: str, todo_update: ToDoItem):
-    try:
-        expression_values = {
-            ':t': todo_update.title,
-            ':d': todo_update.description,
-            ':s': todo_update.status,
-            ':u': datetime.utcnow().isoformat().split('.')[0]
-        }
-        expression_names = {
-            '#title': 'title',
-            '#desc': 'description',
-            '#status': 'status',
-            '#updated_at': 'updated_at'
-        }
-        update_expression = "set #title = :t, #desc = :d, #status = :s, #updated_at = :u"
-
-        if "due_date" in todo_update:
-            update_expression += ", #due_date = :dd"
-            expression_values[':dd'] = todo_update.due_date.isoformat()
-            expression_names['#due_date'] = 'due_date'
-
-        response = table.update_item(
-            Key={'id': id},
-            UpdateExpression=update_expression,
-            ExpressionAttributeValues=expression_values,
-            ExpressionAttributeNames=expression_names,
-            ReturnValues="UPDATED_NEW"
-        )
-
-        # Now retrieve the updated item and return it to ensure it was updated
-        updated_item = table.get_item(Key={'id': id}).get('Item')
-        print(updated_item)
-        return updated_item
-    except ClientError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    return db.update_todo(id, todo_update)
 
 @app.delete("/todos/{id}")
 async def delete_todo(id: str):
-    try:
-        table.delete_item(Key={'id': id})
-        return {"message": "Item deleted successfully"}
-    except ClientError as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    db.delete_todo(id)
